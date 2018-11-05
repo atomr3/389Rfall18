bbecc4f0178cd3d9292f7eff0bb95ad13a489a19
Anand Raghu <Anand@wireless-10-105-3-15.umd.edu>
2018-11-04 23:55:55 -0500

    push stuff

--- week/8/stub.py
+++ week/8/stub.py
@@ -16,16 +16,11 @@ VERSION = 1
 if len(sys.argv) < 2:
     sys.exit("Usage: python2 stub.py input_file.fpff")
 
-# Normally we'd parse a stream to save memory, but the FPFF files in this
-# assignment are relatively small.
+
 with open(sys.argv[1], 'rb') as fpff:
     data = fpff.read()
 
-# Hint: struct.unpack will be VERY useful.
-# Hint: you might find it easier to use an index/offset variable than
-# hardcoding ranges like 0:8
-magic, version = struct.unpack("<LL", data[0:8])
-
+magic, version, time, author, num_sections = struct.unpack("<LLL8sL", data[0:24])
 if magic != MAGIC:
     bork("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
 
@@ -35,8 +30,74 @@ if version != VERSION:
 print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
+print("TIMESTAMP: %d" % int(time))
+print("AUTHOR: %s" % author)
+print("NUM SECTIONS: %d" % num_sections)
 
-# We've parsed the magic and version out for you, but you're responsible for
-# the rest of the header and the actual FPFF body. Good luck!
 
 print("-------  BODY  -------")
+offset = 24
+secs = 0
+while offset < len(data):
+    secs+= 1
+    
+    print("SECTION")
+    stype, slen = struct.unpack("<LL", data[offset:offset+8])
+    offset += 8
+    if stype == 1:
+        print("Type : png");
+        f = open('output.png', 'w')
+        f.write(str(data[offset:offset+slen]))
+        offset += slen
+    elif stype == 2:
+    	# array of dwords
+    	num = slen/8
+        print("Type : dwords")
+        print("Number dwords : %d" % (num))
+        words = []
+        for x in xrange(0,num):
+        	words.append(struct.unpack("<Q", data[offset:offset+8])[0])
+        	offset += 8
+        	pass
+        print(str(words))
+    elif stype == 3:
+        print("Type : utf8")
+        print("Length : %d" % slen)
+        print(data[offset:offset+slen].decode("utf-8"))
+        offset += slen
+    elif stype == 4:
+        print("Type : array of doubles")
+        num = slen/8
+        print("Num doubles : %d" % num)
+        d = []
+        for x in xrange(0,num):
+        	d.append(struct.unpack("<d", data[offset:offset+8]))
+        	offset+=8
+        	pass
+        print(str(d))
+    elif stype == 5:
+        print("Type : array of words")
+        num = slen/4
+        print("Length : %d" % num)
+        w = []
+        for x in xrange(0,num):
+        	w.append(struct.unpack("<L",data[offset:offset+4])[0])
+        	offset+=4
+        	pass
+        print(str(w))
+    elif stype == 6:
+        print("Type : coordinates")
+        print(struct.unpack("<dd", data[offset:offset+16]))
+        offset += 16
+    elif stype == 7:
+        print("Type : reference section")
+        print(struct.unpack("<I", data[offset:offset+4])[0])
+        offset += 4
+    elif stype == 9:
+        print("Type : ascii")
+        print("Length : %d" % slen)
+        print(data[offset:offset+slen])
+        offset += slen
+    else:
+        bork("No match with section type, you borked up fool")
+    print("\n")
--- week/8/writeup/README.md
+++ week/8/writeup/README.md
@@ -29,12 +29,71 @@ Digital acknowledgement of honor pledge: Anand Raghu
 ### Part 2 (55 Pts)
 
 *Report your answers to the questions about parsing update.fpff below.*
-1.
+1. Generated on Oct. 25 2018 @ 12:40:07 am
 
-2.
+2. authored by `laz0rh4x`
 
-3.
+3. Says it has `9` sections, really has `11`
 
-4.
+4. 
+SECTION
+Type : ascii
+Length : 51
+Call this number to get your flag: (422) 537 - 7946
 
-5.
+
+SECTION
+Type : array of words
+Length : 15
+[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9]
+
+
+SECTION
+Type : coordinates
+(38.99161, -77.02754)
+
+
+SECTION
+Type : reference section
+1
+
+
+SECTION
+Type : ascii
+Length : 60
+The imfamous security pr0s at CMSC389R will never find this!
+
+
+SECTION
+Type : ascii
+Length : 991
+The first recorded uses of steganography Can be traced back to 440 BC when Herodotus Mentions two exampleS in his Histories.[3] Histicaeus s3nt a message to his vassal, Arist8goras, by sha9ving the hRead of his most trusted servan-t, "marking" the message onto his scal{p, then sending him on his way once his hair had rePrown, withl the inastructIon, "WheN thou art come to Miletus, bid _Aristagoras shave thy head, and look thereon." Additionally, demaratus sent a warning about a forthcoming attack to Greece by wrIting it dirfectly on the wooden backing oF a wax tablet before applying i_ts beeswax surFace. Wax tablets were in common use then as reusabLe writing surfAces, sometimes used for shorthand.In his work Polygraphiae Johannes Trithemius developed his so-called "Ave-Maria-Cipher" that can hide information in a Latin praise of God. "Auctor Sapientissimus Conseruans Angelica Deferat Nobis Charitas Gotentissimi Creatoris" for example contains the concealed word VICIPEDIA.[4}
+
+SECTION
+Type : coordinates
+(38.9910941, -76.9328019)
+
+
+SECTION
+Type : png
+
+
+SECTION
+Type : ascii
+Length : 22
+AF(saSAdf1AD)Snz**asd1
+
+
+SECTION
+Type : ascii
+Length : 45
+Q01TQzM4OVIte2gxZGQzbi1zM2N0MTBuLTFuLWYxbDN9
+
+
+
+SECTION
+Type : dwords
+Number dwords : 6
+[4, 8, 15, 16, 23, 42]
+
+5. Flags : `CMSC389R-{c0rn3rst0ne_airlin3s_to_the_m00n}` from png, `CMSC389-{h1dd3n-s3ct10n-1n-fil3}` decoded from base64 from the dwords, and `CMSC389R-{PlaIN_dIfF_FLAG}` from the steg paragraph description.
