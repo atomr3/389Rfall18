28b61fad4db2089856932cd1fc857501f2dcba39
Anand Raghu <Anand@wireless-10-105-5-158.umd.edu>
2018-11-22 23:52:01 -0500

    turn this in 9 mins before deadline

--- week/10/writeup/README.md
+++ week/10/writeup/README.md
@@ -12,11 +12,14 @@ Digital acknowledgement of honor pledge: Anand Raghu
 
 ### Part 1 (70 Pts)
 For this part, I automated socket connections to the server, after `nc`ing in, because I quickly realized that manually interacting would be kind of painful, due to the amount of copy and pasting within the terminal.
+For step 1, I simply sent my message to be signed via the socket, and grabbed the hash from the response. I used this to initialize the state of the md5 we have, and then updated it with my malicious message 'InfoSci'. Then I printed out the new false hash which represents the supposed payload.
+
+For step 2, we know that the secret size is between 6-16, so we iterate through that range. Size of 64 minus 8 minus 1 for the preceding 1 byte, which in total is 55. Then we subtract the message length and the secret size, which yields the padding. With this, we can send the payload which is message + padding + malicious joined together. In order to recieve the full data, I put in a sleep because I was not recieving the entirity of the message. I then just simply checked whether there was the beginning of a flag contained within the message text, and printed out the correct message. 
 
 
 Flag `CMSC389R-{i_still_put_the_M_between_the_DV}`
 
 
 ### Part 2 (30 Pts)
-There's basically 3 commands that you really need for this part, one to generate a key - `gpg --gen-key`, one to import the secret key given - `gpg --import filename`, and then one to encrypt the final message - `gpg --output filename.private --encrypt --recipient p@csec.umiacs.umd.edu message.`
+There's basically 3 commands that you really need for this part, one to generate a key - `gpg --gen-key`, one to import the secret key given - `gpg --import filename`, and then one to encrypt the final message - `gpg --output filename.private --encrypt --recipient president@csec.umiacs.umd.edu filename.`
 
--- week/10/writeup/stub.py
+++ week/10/writeup/stub.py
@@ -2,39 +2,57 @@
 # from the git repo
 import md5py
 import socket
+import struct
+import time
 
-host = "142.93.118..186"
+host = "142.93.118.186"
 port = 1234
 
 #####################################
 ### STEP 1: Calculate forged hash ###
 #####################################
 
-s = socket.socket(socket.AF_NET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
 s.recv(1024)
 
+# go to sign some data
+s.send('1\n')
+s.recv(1024)
 
-message = ''    # original message here
-legit = ''      # a legit hash of secret + message goes here, obtained from signing a message
+# make notary sign something and grab hash
+s.send("Hello World\n")
+data = s.recv(1024)
+# grab last value of array which is hash
+legit = data.split()[-1]     # a legit hash of secret + message goes here, obtained from signing a message
+print("Real: " + legit) 
+
+# i guess we need message after all, didn't pay too much attention to part 2 :/
+# no newline char
+message = "Hello World"
+
 
 # initialize hash object with state of a vulnerable hash
 fake_md5 = md5py.new('A' * 64)
 fake_md5.A, fake_md5.B, fake_md5.C, fake_md5.D = md5py._bytelist2long(legit.decode('hex'))
 
-malicious = ''  # put your malicious message here
+malicious = 'InfoSci'  # put your malicious message here
 
 # update legit hash with malicious message
 fake_md5.update(malicious)
 
 # fake_hash is the hash for md5(secret + message + padding + malicious)
 fake_hash = fake_md5.hexdigest()
-print(fake_hash)
+print("Fake: " + fake_hash)
+
+# done
 
 
 #############################
 ### STEP 2: Craft payload ###
 #############################
+# menu
+s.recv(1024)
 
 # TODO: calculate proper padding based on secret + message
 # secret is <redacted> bytes long (48 bits)
@@ -44,13 +62,39 @@ print(fake_hash)
 # (i.e. 20 char msg = 160 bits = 0xa0 = '\xa0\x00\x00\x00\x00\x00\x00\x00\x00')
 # craft padding to align the block as MD5 would do it
 # (i.e. len(secret + message + padding) = 64 bytes = 512 bits
-padding = ''
+
+# 64-8-1 = 55
+# you would pad with 1 '1', then potentially 55 0's minus the secret and our own message
+for x in xrange(6,16):
+	l = len(message)
+	l2 = struct.pack('<Q', ((x+l)*8))
+	padding = '\x80' + ('\x00'*(55-l-x)) + l2
+	payload = message + padding + malicious
+	
+	# test signature validity menu
+	s.send("2\n")
+	s.recv(1024)
+
+	# send fake and payload
+	s.send(fake_hash + "\n")
+	s.recv(1024)
+	s.send(payload + "\n")
+	# too fast for the server :(, have to wait a bit for the response
+	time.sleep(2)
+	data = s.recv(2048)
+	if 'CMSC389R' in data:
+		print(data)
+		break
+		pass
+	# print(data)
+
+
+	pass
 
 # payload is the message that corresponds to the hash in `fake_hash`
 # server will calculate md5(secret + payload)
 #                     = md5(secret + message + padding + malicious)
 #                     = fake_hash
-payload = message + padding + malicious
 
 # send `fake_hash` and `payload` to server (manually or with sockets)
 # REMEMBER: every time you sign new data, you will regenerate a new secret!
